6031cbcee39ae341ac878be42983fd02c401cd01
Quoc Nghi <bdqnghi@gmail.com>
2017-10-04 22:44:34 +0800

    update

--- evaluate_token_similarity.py
+++ evaluate_token_similarity.py
@@ -5,6 +5,9 @@ import codecs
 from sklearn.manifold import TSNE
 from gensim.models.keyedvectors import KeyedVectors
 import matplotlib.pyplot as plt
+from node_map import NODE_LIST
+
+
 test_words = ["BinOp","If","For","Break","While","FunctionDef"]
 
 vectors = KeyedVectors.load_word2vec_format("./data/vectors.txt",binary=False)
